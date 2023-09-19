ebbc2d29ffb083254027d5226363a80cba9cda5a
Bigbrainium <prashunregmi15@gmail.com>
2023-09-19 09:40:16 -0400

    lab_04_Part_3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int winterCost;
+        int springCost;
+        int summerCost;
+        int fallCost;
+
+
+        System.out.println("What is your winter maintenance cost");
+        winterCost = scan.nextInt();
+        System.out.println("What is your spring maintenance cost");
+        springCost = scan.nextInt();
+        System.out.println("What is your summer maintenance cost");
+        summerCost = scan.nextInt();
+        System.out.println("What is your fall maintenance cost");
+        fallCost = scan.nextInt();
+
+        int totalCost = winterCost + summerCost + springCost + fallCost;
+
+        System.out.println("");
+        System.out.println("Your yearly maintenance cost is: $" + totalCost);
     }
 }
